ad4dfd58d16c8a17086fc362ddc62682044eea26
Portismouth <ozzy.gonzalez@gmail.com>
2018-04-13 23:33:27 -0500

    new project and setting up boilerplate with sass-loader

---
+++ basic-boilerplate/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles/styles.css';
+import 'normalize.css/normalize.css'
+
+ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById("root"));
---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EditExpensePage = props => {
+  return (
+    <div>Editing the expense with the ID of {props.match.params.id}</div>
+  );
+};
+
+export default EditExpensePage;
